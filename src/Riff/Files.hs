6d7b67b61e2754a602bf30148438c2377ae808e7
Steven Meunier <code@solaryeti.com>
2015-08-23 13:27:14 +0200

    Documentation

--- src/Riff/Files.hs
+++ src/Riff/Files.hs
@@ -1,3 +1,5 @@
+-- | Directory listing functions
+
 module Riff.Files
        ( files
        , dirs
@@ -8,31 +10,23 @@ import System.Directory (getDirectoryContents, canonicalizePath)
 import System.FilePath ((</>))
 import System.Posix.Files (isRegularFile, isDirectory, getFileStatus, FileStatus)
 
+-- | List the regular files in a given a directory.
 files :: FilePath -> IO [FilePath]
 files = flip filteredLs isRegularFile
 
+-- | List the directory names one level deep under a given parent directory.
 dirs :: FilePath -> IO [FilePath]
 dirs = flip filteredLs isDirectory
 
-
+-- | Filter out '.' and '..' from a directory listing.
 filterSpecial :: Monad m => [FilePath] -> m [FilePath]
 filterSpecial = filterM (\x -> return $ x /= "." && x /= "..")
 
+-- | List a filtered directory listing for a given path.
 filteredLs :: FilePath -> (FileStatus -> Bool) -> IO [FilePath]
 filteredLs x p = ls x >>= filterM (getFileStatus >=> return . p)
 
+-- | List the absolute path for all files in a given directory.
 ls :: FilePath -> IO [FilePath]
 ls x = getDirectoryContents x >>= filterSpecial >>= absPaths
   where absPaths = mapM (canonicalizePath . (x </>))
--- Let's handle the IO exceptions in main
--- import Control.Exception (try)
--- import System.IO.Error
---   do
---   dirContents <- try (getDirectoryContents x)  :: IO (Either IOError [FilePath])
---   case dirContents of
---     Left _ -> Nothing
---     Right fs -> Just $ absPaths (filterSpecial' fs)
---       where absPaths = mapM (canonicalizePath . (x </>))
-
--- filterSpecial' :: [FilePath] -> [FilePath]
--- filterSpecial' = filter (\x -> x /= "." && x /= "..")
--- src/Riff/Sanitize.hs
+++ src/Riff/Sanitize.hs
@@ -1,20 +1,30 @@
-module Riff.Sanitize where
+-- | String sanitation
+
+module Riff.Sanitize
+       ( removeInvalid
+       , removeDupUnderscore
+       , validChars
+       ) where
 
 import Data.Char (intToDigit)
 import Data.List (group)
 
+-- | A list of valid characters for file names.
 validChars :: String
 validChars = concat [ ['a'..'z']
                     , ['A'..'Z']
                     , map intToDigit [0..9]
                     , "_." ]
 
+-- | Replace any characters in the string that are not part of 'validChars'
+-- with an underscore.
 removeInvalid :: String -> String
 removeInvalid = foldr invalidToUnderscore ""
     where invalidToUnderscore x ys
             | x `elem` validChars = x : ys
             | otherwise = '_' : ys
 
+-- | Remove duplicate underscores from a string.
 removeDupUnderscore :: String -> String
 removeDupUnderscore = concatMap squash . group
   where squash ('_':_) = "_"
